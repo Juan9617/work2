9044f3e340a71241233ce4aaa6cef36a634ef417
juan9617 <juanse.9617@gmail.com>
2023-04-16 19:36:22 -0500

    modificacion index.ts

--- NuevaCarpeta/src/index.ts
+++ NuevaCarpeta/src/index.ts
@@ -1,3 +1,17 @@
-function crearAlerta(){
-    alert("Hola mundo")
-}
+const form = document.getElementById ('form') as HTMLFormElement;
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    console.log("Ingreso al formulario");
+
+    const tipoDocumento = document.getElementById('tipoDocumento') as HTMLFormElement;
+    const numeroDocumento = document.getElementById('numeroDocumento') as HTMLFormElement;
+    const nombre = document.getElementById('nombre') as HTMLFormElement;
+    const apellido = document.getElementById('apellido') as HTMLFormElement;
+    const celular = document.getElementById('celular') as HTMLFormElement;
+    const email = document.getElementById('coreo') as HTMLFormElement;
+    const linkedin = document.getElementById('usuarioLinkedin') as HTMLFormElement;
+    const github = document.getElementById('usuarioGit') as HTMLFormElement;
+
+    console.log(tipoDocumento.value);
+})

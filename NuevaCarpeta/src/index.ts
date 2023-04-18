300f7073906fe2064afe656578833557f43a23c2
juan9617 <juanse.9617@gmail.com>
2023-04-18 17:52:27 -0500

    modificacion trabajo

--- NuevaCarpeta/src/index.ts
+++ NuevaCarpeta/src/index.ts
@@ -5,15 +5,40 @@ btnGuardar.addEventListener ('click', (e) =>{
         e.preventDefault();
         console.log("Ingreso al formulario");
     
-        const tipoDocumento = document.getElementById('tipoDocumento') as HTMLFormElement;
-        const numeroDocumento = document.getElementById('numeroDocumento') as HTMLFormElement;
-        const nombre = document.getElementById('nombre') as HTMLFormElement;
-        const apellido = document.getElementById('apellido') as HTMLFormElement;
+        const tipoIdentificacion = document.getElementById('tipoIdentificacion') as HTMLFormElement;
+        const numeroIdentificacion = document.getElementById('numeroIdentificacion') as HTMLFormElement;
+        const nombres = document.getElementById('nombres') as HTMLFormElement;
+        const apellidos = document.getElementById('apellidos') as HTMLFormElement;
         const celular = document.getElementById('celular') as HTMLFormElement;
         const email = document.getElementById('coreo') as HTMLFormElement;
         const linkedin = document.getElementById('usuarioLinkedin') as HTMLFormElement;
         const github = document.getElementById('usuarioGit') as HTMLFormElement;
     
-        console.log(tipoDocumento.value);
+        console.log(tipoIdentificacion.value);
 })
 
+let raw = JSON.stringify({
+        "tipoIdentificacion": tipoIdentificacion.value,
+        "numeroIdentificacion": numeroIdentificacion.value,
+        "nombres": nombres.value,
+        "apellidos": apellidos.value,
+        "celular": celular.value,
+        "correo": email.value,
+        "linkedin": linkedin.value,
+        "github": github.value,
+      });
+      
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+      
+      //fetch("https://apiestudiantes.maosystems.dev/estudiantes", requestOptions)//
+        //.then(response => response.text())//
+        //.then(result => console.log(result))//
+        //.catch(error => console.log('error', error));//
+      
+
+       
